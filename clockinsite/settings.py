3c8d498201e429f4b2145ae7035b377e14dea199
Pythonic2023 <124443031+Pythonic2023@users.noreply.github.com>
2025-08-24 19:48:04 -0400

    Included clockinapp in installed apps. Serves test page.

--- clockinsite/settings.py
+++ clockinsite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from clockinsite import secrets
+from clockinsite.secrets import SECRET
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secrets
+SECRET_KEY = SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clockinapp'
 ]
 
 MIDDLEWARE = [
